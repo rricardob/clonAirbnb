1d245652d1e527cab4326ce70280473c28e9f5f9
Ricardo Bueno <ricardo.bueno@osf.pe>
2021-07-18 00:17:48 -0500

    se Ãinserta el menu y el footer en la web de inicio y la de experiencias online

--- js/mainFunctions.js
+++ js/mainFunctions.js
@@ -1,6 +1,6 @@
 $(document).ready(() => {
     console.log('ready');
-    $('.navbar_wraper').load('/common/navbar.html');
-    //$('.footer_wraper').load('/components/footer.html');
+    $('.navbar_wraper').load('./common/navbar.html');
+    $('.footer_wraper').load('./common/footer.html');
 
 });
